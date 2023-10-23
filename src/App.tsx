63724a51d130a39345d48b75b084acad92eb265a
Filipe Gianino <filipegianinom@gmail.com>
2023-10-23 16:09:06 -0300

    menu bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-
+import Navbar from '@/scenes/navbar'
+import { useState } from 'react'
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<string>('Home')
 
-  return (
-    <div className='app'> app
-      
+    return <div className="app bg-gray-20"> 
+    <Navbar 
+    selectedPage = {selectedPage}
+    setSelectedPage = {setSelectedPage}
+    />
     </div>
-  )
+    
 }
 
  
